c58e612d0236bb3a73fb43c7de707ce70159636c
Nicholas Young <nicholas@nicholaswyoung.com>
2011-01-13 13:30:27 -0600

    Almost there.

--- js/site.js
+++ js/site.js
@@ -1,5 +1,13 @@
-var html5Elements = ['section'];
-for(var i = 0; i < html5Elements.length; i++) {
-	document.createElement(html5Elements[i]);
-}
-VideoJS.setupAllWhenReady();
+$(function() {
+	VideoJS.setupAllWhenReady();
+	var getVars = [], hash;
+	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	for(var i = 0; i < hashes.length; i++) {
+		hash = hashes[i].split('=');
+		getVars.push(hash[0]);
+		getVars[hash[0]] = hash[1];
+	}
+	if(getVars.action == 'confirm') {
+		$('section#messages').slideDown();
+	}
+});
